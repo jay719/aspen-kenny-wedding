90d1a6edefb76f0b6c235f06af7e837634aa932f
Javaria Brascom <jbrascom@quantummetric.com>
2022-02-01 22:33:31 -0700

    added submit button

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,9 @@ export default function Home() {
           <div className='form-group'>
             <label>Email </label>
             <input type='email' />
+            
           </div>
+          <input type='submit' />
         </div>
         
       </form>
